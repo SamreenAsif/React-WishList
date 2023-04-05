24a6b5d483a28cd9b9b309cb484ae173c6105e25
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-04-05 11:49:39 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { WishList } from './WishList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WishList/>
+    </div>
+  );
+}
+
+export default App;
